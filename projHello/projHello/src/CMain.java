132c4f1dc066c48e8193a517a498d5c263d6cf01
je355 <120763503+je355@users.noreply.github.com>
2023-08-25 22:46:22 +0900

    Java Hello
    Java first class

---
+++ projHello/projHello/src/CMain.java
@@ -0,0 +1,8 @@
+public class CMain {
+    public static void main(String[] args)
+    {
+        System.out.println("1. Hello world!");
+        System.out.println("2. My name is bch");
+        System.out.println("3. My hobby is sleeping");
+    }
+}
