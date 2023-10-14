69082ba6f16601a8fc5b96b81d9ad5e2a6a630f3
je355 <120763503+je355@users.noreply.github.com>
2023-10-14 17:32:35 +0900

    java basic

--- projBchThreadDos/src/CJobs.java
+++ projBchThreadDos/src/CJobs.java
@@ -2,8 +2,14 @@ public class CJobs {
     static private int mJobNum = 0;
     //static이 붙으면 class 변수가 됨 <---> 인스턴스 변수(그냥 변수, 오브젝트변수 - new 필요)
 
-    static public int mGetJobNum(){
+    static synchronized public int mGetJobNum(){
+        //synchronized영역 하나의 thread만 들어갈 수 있게 만ㄷ르어줌
         mJobNum++;
+        try {
+            Thread.sleep(500); //지연시키기
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         return mJobNum;
     }
 }
--- projBchThreadDos/src/Main.java
+++ projBchThreadDos/src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        // System.out.println("Hello world!");
         CWorker objWork1 = new CWorker("호준");
 
-        Thread th1 = new Thread(()->{
-            while(objWork1.mArrJobs.size()< objWork1.MAXJOBCNT) {
+        Thread th1 = new Thread((/*이름 없는 함수 파라미터 던저준다는데 그게 뭔 소린지 모르겠음*/) -> {
+            while(objWork1.mArrJobs.size() < objWork1.MAXJOBCNT) {
                 objWork1.mWork();
-                objWork1.mShow();
-
+//                objWork1.mShow();
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
@@ -14,7 +14,66 @@ public class Main {
                 }
             }
         });
+
         th1.setName("thread호준");
         th1.start();
+
+        //-----------------------------호준 null--------------------------------
+
+        CWorker objWork2 = new CWorker("호준null");
+
+        Thread th2 = new Thread(() -> {
+            while(objWork2.mArrJobs.size() < objWork2.MAXJOBCNT) {
+                objWork2.mWork();
+//                objWork2.mShow();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        th2.setName("thread호준null");
+        th2.start();
+        //-----------------------------호준 none--------------------------------
+
+        CWorker objWork3 = new CWorker("호준none");
+
+        Thread th3 = new Thread(() -> {
+            while(objWork2.mArrJobs.size() < objWork3.MAXJOBCNT) {
+                objWork3.mWork();
+//                objWork3.mShow();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        th3.setName("thread호준none");
+        th3.start();
+
+        //-----------------------------스레드 monitor--------------------------------
+
+        Thread thMonitor = new Thread(() -> {
+            while (th1.isAlive() || th2.isAlive() || th3.isAlive()) {
+                System.out.println("---------------------------------");
+                objWork1.mShow();
+                objWork2.mShow();
+                objWork3.mShow();
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        thMonitor.setName("모니터스레드");
+        thMonitor.start();
+
     }
+
 }
